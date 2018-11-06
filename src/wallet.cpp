b1f334764d27af763875de53c462539b0a3317b9
BitBlocks <Bitblocksproject@gmail.com>
2018-11-06 13:45:56 -0200

    Fix imagens and transactions
    Accept 0 fee transactions with more size  and fix QT image bug.

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -2293,8 +2293,7 @@ bool CWallet::CreateTransaction(const vector<pair<CScript, CAmount> >& vecSend,
                 // If we made it here and we aren't even able to meet the relay fee on the next pass, give up
                 // because we must be at the maximum allowed fee.
                 if (nFeeNeeded < ::minRelayTxFee.GetFee(nBytes)) {
-                    strFailReason = _("Transaction too large for fee policy");
-                    return false;
+                   
                 }
 
                 if (nFeeRet >= nFeeNeeded) // Done, enough fee included
